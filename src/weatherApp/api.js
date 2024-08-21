3a043b498fd2d93f34dc481b01fbb00c286b8f68
Muhammad Abdullah <abdullahimran8875@gmail.com>
2024-08-21 17:47:19 +0500

    Update api.js

--- src/weatherApp/api.js
+++ src/weatherApp/api.js
@@ -1,4 +1,4 @@
-const apiKey = "544e0f69c9cccccab9abd812d7fa8bb8";
+const apiKey = "xxxxxxxxxxxxxxxxxxxx";
  export const getWeather = async (city) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     const response = await fetch(url);
